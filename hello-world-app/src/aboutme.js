9bbe4c101541f8ee28b8824ca8e106ec3265ae8f
Brittany Woods <brittanywoods@brittanys-mbp.lan>
2022-12-25 17:24:49 -0600

    Initial commit, add hello world app, add aboutme.js and app custom

---
+++ hello-world-app/src/aboutme.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutMe() {
+    return(
+        <div>
+            <h1>Hi, I'm Brittany</h1>
+            <p>I'm trying to learn react</p>
+            <p>It doesn't seem too bad so far</p>
+        </div>
+    )
+}
+
+export default AboutMe;
---
+++ hello-world-app/src/helloworld.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HelloWorld() {
+    return(
+        <div>
+            <h1>Hello, World!</h1>
+            <ul>
+                <li>Hello</li>
+                <li>World</li>
+            </ul>
+        </div>
+    )
+}
+
+export default HelloWorld;
